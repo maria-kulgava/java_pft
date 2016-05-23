d7f7d725eb864c68b62a8ab4cad1d494df094fb0
maria-kulgava <mashak2008@list.ru>
2016-05-23 10:53:15 -0400

    Вычисление расстояния между двумя точками №2
    Добавлено вычисление расстояния между двумя точками с помощью метода в
    классе Point

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -3,12 +3,25 @@
  */
 public class Point {
 
-  public double x;
-  public double y;
+  public double x1;
+  public double y1;
+  public double x2;
+  public double y2;
 
   public Point (double x, double y){
-    this.x = x;
-    this.y = y;
+    this.x1 = x;
+    this.y1 = y;
+  }
+
+  public Point (double x1, double y1, double x2, double y2){
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  public double distance() {
+    return Math.sqrt(Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2));
   }
 
 }
