893e88bab748609b9d78d7f72252a0db005821aa
maria-kulgava <mashak2008@list.ru>
2016-05-23 10:16:01 -0400

    Вычисление расстояния между двумя точками №1
    Вычисление расстояния между двумя точками с помощью метода:
    public static double distance(Point p1, Point p2)

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Admin on 5/23/2016.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point (double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+}
