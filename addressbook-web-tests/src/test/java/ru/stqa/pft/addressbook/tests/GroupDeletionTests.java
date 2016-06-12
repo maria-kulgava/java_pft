d755b66480c2974c9c594f37eed062330d037ea6
maria-kulgava <mashak2008@list.ru>
2016-06-12 18:58:31 -0400

    Для теста удаление групп добавлено сравнение списков до и после удаления группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -26,6 +26,9 @@ public class GroupDeletionTests extends TestBase {
 
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(after, before);
   }
 
 }
