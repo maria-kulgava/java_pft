24ccdac7d2d80abb7f8a348815dd37de654ee011
maria-kulgava <mashak2008@list.ru>
2016-06-20 11:26:59 -0400

    Добавлено хэширование для тестов групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -33,8 +33,9 @@ public class GroupModificationTests extends TestBase {
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("test2").withHeader("test3").withFooter("test4");;
     app.group().modify(group);
+
+    assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
-    assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
