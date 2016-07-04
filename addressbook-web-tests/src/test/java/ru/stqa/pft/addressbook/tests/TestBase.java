80f4e129d02b4fa46d33f69a6dabdb4aa9e69f74
maria-kulgava <mashak2008@list.ru>
2016-07-04 06:17:10 -0400

    Реализовано протоколирование
    Добавлен файл logback.xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,16 +1,23 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by Admin on 6/1/2016.
  */
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class); // добавление логгера (пакет org.slf4j) для указанного класса
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
@@ -20,9 +27,19 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite (alwaysRun = true) // опция (alwaysRun = true) - что бы браузер останавливался всегда
   public void tearDown() {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod (alwaysRun = true) // опция (alwaysRun = true) - чтобы метод отрабатывал всегда, даже если тест падает
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
   }
 
 }
