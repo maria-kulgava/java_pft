7e29eb49cd41bf01e0fc52833902aef6d5bfa9a2
maria-kulgava <mashak2008@list.ru>
2016-07-17 17:54:41 -0400

    Реализовано получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,20 +1,27 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
 /**
  * Created by Admin on 7/16/2016.
  */
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver driver;
+public class RegistrationHelper extends HelperBase {
 
-  public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
-    driver = app.getDriver();
+   public RegistrationHelper(ApplicationManager app) {
+    super(app);
   }
 
   public void start(String username, String email) {
     driver.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+    type(By.name("username"), username);
+    type(By.name("email"), email);
+    click(By.cssSelector("input[value='Signup']"));
+  }
+
+  public void finish(String confirmationLink, String password) {
+    driver.get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    click(By.cssSelector("input[value='Update User']"));
   }
 }
